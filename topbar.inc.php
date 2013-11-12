12f3cb8b2f48e2257793161fe7f58dfd753a4652
Salvo Adriano Zappalà <salvozpp@yahoo.it>
2013-11-12 12:00:45 +0100

    Disable notification icon

--- topbar.inc.php
+++ topbar.inc.php
@@ -3,7 +3,7 @@
 		<span id="logo"><a href="index.php"><?= TITLE ?></a></span>
 		<a href="friend-requests.php"><img class="top-button <?= ($topBarSelectedIcon == 1) ? 'selected' : '' ?>" src="img/friend-requests.png"></a>
 		<a href="messages.php"><img class="top-button <?= ($topBarSelectedIcon == 2) ? 'selected' : '' ?>" src="img/messages.png"></a>
-		<img class="top-button <?= ($topBarSelectedIcon == 3) ? 'selected' : '' ?>" src="img/notifications.png">
+		<!--<img class="top-button" src="img/notifications.png">-->
 		<form method="get" action="search.php" style="display: inline-block">
 			<input id="search" name="fullname" type="text">
 		</form>
